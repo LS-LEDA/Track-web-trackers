7477848202cef4e8d0303a4676bc8bc55d78e70f
TheDidi <80844791+TheDidi@users.noreply.github.com>
2024-01-09 21:05:37 +0100

    accordion

---
+++ front/src/components/Accordion.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="accordion" @click="toggleAccordion">
+        <img :src="image" alt="Accordion image" class="accordion-img" />
+        <h2 class="accordion-title">{{ title }}</h2>
+        <span class="accordion-state-icon" :class="{ 'rotate': isOpen }">&#9660;</span>
+    </div>
+    <div v-show="isOpen" class="accordion-content">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import { strings } from '../assets/strings';
+
+export default {
+    props: {
+        image: String,
+        title: String
+    },
+    data() {
+        return {
+            isOpen: false,
+            language: 'es'
+        };
+    },
+    methods: {
+        toggleAccordion(){
+            this.isOpen = !this.isOpen;
+        }
+    },
+};
+
+</script>
+
+<style lang="scss" scoped> /**scoped vol dir que les regles en auqets style sol s'apliquen aquí */
+* {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+.accordion {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    padding: 10px;
+    border-radius: 10px;
+    transition: 0.1s;
+
+    &:hover {
+        background-color: grey;
+    }
+
+    .accordion-img {
+        width: 20px;
+    }
+
+    .accordion-title {
+        margin-left: 10px;
+    }
+}
+</style>
